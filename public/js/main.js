5cb0df503eab4fe5eabae5f79472c69024ce0497
Calvin <ca7vin@gmail.com>
2022-01-06 16:13:57 +0100

    Double Zoom and Typing

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,11 @@
+// START TYPING SCRIPT
 const typedTextSpan = document.querySelector(".typed-text")
 
 
-const textArray = ["CALVIN", "#developpeur", "#geek", "#graphiste", "#Molengeekois"];
-const typingDelay = 200;
-const erasingDelay = 100;
-const newTextDelay = 2000;
+const textArray = ["CALVIN", "27 years old", "#Belgian", "#developer", "#graphist", "#Molengeekois"];
+const typingDelay = 150;
+const erasingDelay = 50;
+const newTextDelay = 1500;
 let textArrayIndex = 0;
 let charIndex = 0;
 
@@ -35,3 +36,75 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(type, newTextDelay + 250);
 })
+
+// TYPE SCRIPT 2
+
+// const typedTextSpan2 = document.querySelector(".typed-text2")
+
+
+// const textArray2 = ["ca7vin@gmail.com"];
+// const typingDelay2 = 150;
+// const erasingDelay2 = 50;
+// const newTextDelay2 = 1500;
+// let textArrayIndex2 = 0;
+// let charIndex2 = 0;
+
+// function type2() {
+//     if (charIndex2 < textArray2[textArrayIndex2].length) {
+//         typedTextSpan2.textContent += textArray2[textArrayIndex2].charAt(charIndex2);
+//         charIndex2++;
+//         setTimeout(type2, typingDelay2);
+//     } else {
+//         setTimeout(erase2, newTextDelay2);
+//     }
+// }
+
+// function erase2() {
+//     if (charIndex2 > 0) {
+//         typedTextSpan2.textContent = textArray2[textArrayIndex2].substring(0,charIndex2 -1)
+//         charIndex2--;
+//         setTimeout(erase2, erasingDelay2)
+//     } else {
+//         textArrayIndex2++;
+//         if (textArrayIndex2 >= textArray.length) {
+//             textArrayIndex2 = 0;
+//         }
+//         setTimeout(type2, typingDelay2 + 1000);
+//     }
+// }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     setTimeout(type2, newTextDelay2 + 250);
+// })
+
+
+// END TYPING SCRIPT
+// START ZOOM SCRIPT
+
+const zoomElement = document.querySelector(".workspace");
+let zoom = 1;
+const zoomSpeed = 0.1
+
+document.addEventListener("wheel", function(e) {
+    if (e.deltaY > 0) {
+        zoomElement.style.transform = `scale(${(zoom += zoomSpeed)})`;
+    } else if (zoomElement.getBoundingClientRect().width >= 30){
+        zoomElement.style.transform = `scale(${(zoom -= zoomSpeed)})`
+    }
+})
+
+// ZOOM SCRIPT 2
+
+// const zoomElement2 = document.querySelector(".workspace2");
+// let zoom2 = 1;
+// const zoomSpeed2 = 0.1
+
+// document.addEventListener("wheel", function(e) {
+//     if (e.deltaY > 0) {
+//         zoomElement2.style.transform = `scale(${(zoom2 -= zoomSpeed2)})`;
+//     } else if (zoomElement2.getBoundingClientRect().width >= 30){
+//         zoomElement2.style.transform = `scale(${(zoom2 += zoomSpeed2)})`
+//     }
+// })
+
+// END ZOOM SCRIPT
